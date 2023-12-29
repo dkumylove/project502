45ba50cf6c53774dac26519d8e38ad181b96e3b9
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-29 14:43:43 +0900

    세팅중

--- src/main/java/org/choongang/commons/Utils.java
+++ src/main/java/org/choongang/commons/Utils.java
@@ -1,28 +1,35 @@
 package org.choongang.commons;
 
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
+import org.springframework.util.StringUtils;
 
 @Component
 @RequiredArgsConstructor
 public class Utils {
 
-    private final HttpServletResponse request;
+    private final HttpServletRequest request;
+    private final HttpSession session;
 
     /**
      * 모바일인지 확인하는 편의기능
      */
     public boolean isMobile() {
+        // 모바일 수동 모드 전화 체크
+        String device = (String)seesion.getAttribute("device");
+        if (StringUtils.hasText(device)) {
+            return device.equals("MOBILE");
+        }
+
+
         // 요청 헤더 : User-Agent
         String ua = request.getHeader("User-Agent");
 
         String pattern = ".*(iPhone|iPod|iPad|BlackBerry|Android|Windows CE|LG|MOT|SAMSUNG|SonyEricsson).*";
 
-        boolean isMobile = ua.matches(pattern);
-
-        return isMobile;
+        return ua.matches(pattern);
     }
 
     public String tpl(String path) {
