aa83316191cfc69364ee5d1cbb414e8fd20dcb85
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-29 12:47:11 +0900

    세팅중

---
+++ src/main/java/org/choongang/commons/Utils.java
@@ -0,0 +1,27 @@
+package org.choongang.commons;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class Utils {
+
+    private final HttpServletResponse request;
+
+    /**
+     * 모바일인지 확인하는 편의기능
+     */
+    public boolean isMobile() {
+        // 요청 헤더 : User-Agent
+        String ua = request.getHeader("User-Agent");
+
+        String pattern = ".*(iPhone|iPod|iPad|BlackBerry|Android|Windows CE|LG|MOT|SAMSUNG|SonyEricsson).*";
+
+        boolean isMobile = ua.matches(pattern);
+
+        return isMobile;
+    }
+}
