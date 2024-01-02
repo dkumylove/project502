f937d9bba4ec94e024dfe93fb8b7ee045ddf59cb
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-01-02 10:37:45 +0900

    member 기본구성

--- src/main/java/org/choongang/member/service/MemberInfoService.java
+++ src/main/java/org/choongang/member/service/MemberInfoService.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+/**
+ * 조회
+ */
 @Service
 @RequiredArgsConstructor
 public class MemberInfoService implements UserDetailsService {
@@ -16,11 +19,15 @@ public class MemberInfoService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        Member member = memberRepository.findByEmail(username)
-                            .orElseGet(() -> memberRepository.findByUserId(username)
-                            .orElseThrow(MemberNotFoundException::new));
+        Member member = memberRepository.findByEmail(username)  // 이메일 조회
+                            .orElseGet(() -> memberRepository.findByUserId(username)  // 아이디 조회
+                            .orElseThrow(() -> new UsernameNotFoundException(username)));  // 없는경우 예외처리
 
-
-        return null;
+        return MemberInfo.builder()
+                .email(member.getEmail())
+                .userId(member.getUserId())
+                .password(member.getPassword())
+                .member(member)
+                .build();
     }
 }
