8a64db3d2f5dbc65ce0e7b58e4893bb9bafe4133
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-01-11 18:48:29 +0900

    게시판 작성중

--- src/main/resources/static/admin/js/board/form.js
+++ src/main/resources/static/admin/js/board/form.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
     const { loadEditor } = commonLib;
 
+
     loadEditor("html_top")
         .then(editor => window.editor1 = editor)
         .catch(err => console.error(err));
@@ -8,14 +9,27 @@ window.addEventListener("DOMContentLoaded", function() {
     loadEditor("html_bottom")
         .then(editor => window.editor2 = editor)
         .catch(err => console.error(err));
+
+    /* 이미지 본문 추가 이벤트 처리 S */
+    const insertImages = document.getElementsByClassName("insert_image");
+    for (const el of insertImages) {
+        el.addEventListener("click", function() {
+            const parentId = this.parentElement.parentElement.id;
+            const editor = parentId.indexOf("html_bottom") == -1 ? editor1 : editor2;
+            const url = this.dataset.url;
+
+            insertImage(editor, url);
+        });
+    }
+    /* 이미지 본문 추가 이벤트 처리 E */
 });
 
 /**
- * 파일 업로드 후 후속 처리 함수
- * @param files
+ * 파일 업로드 후 후속처리 함수
+ *
  */
 function callbackFileUpload(files) {
-    if(!files || files.length == 0) {
+    if (!files || files.length == 0) {
         return;
     }
 
@@ -27,9 +41,10 @@ function callbackFileUpload(files) {
     for (const file of files) {
         const editor = file.location == 'html_bottom' ? editor2 : editor1;
         const target = file.location == 'html_bottom' ? targetBottom : targetTop;
-        editor.execute('insertImage', {source: file.fileUrl});
 
-        /* 템플릿 데이터 치환 s */
+        insertImage(editor, file.fileUrl); // 에디터에 이미지 추가
+
+        /* 템플릿 데이터 치환 S */
         let html = tpl;
         html = html.replace(/\[seq\]/g, file.seq)
             .replace(/\[fileName\]/g, file.fileName)
@@ -39,7 +54,21 @@ function callbackFileUpload(files) {
         const fileBox = dom.querySelector(".file_tpl_box");
         target.appendChild(fileBox);
 
-        /* 템플릿 데이터 치환 e */
 
+        const el = fileBox.querySelector(".insert_image")
+        if (el) {
+            // 이미지 본문 추가 이벤트
+            el.addEventListener("click", () => insertImage(editor, file.fileUrl));
+        }
+
+        /* 템플릿 데이터 치환 E */
     }
+}
+
+/**
+ * 에디터에 이미지 추가
+ *
+ */
+function insertImage(editor, source) {
+    editor.execute('insertImage', { source });
 }
