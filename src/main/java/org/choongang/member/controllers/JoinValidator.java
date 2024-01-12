19aaeaca7f452b4b5fbde7002b0037ef735bcf1c
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-01-12 09:28:58 +0900

    이메일 인증

--- src/main/java/org/choongang/member/controllers/JoinValidator.java
+++ src/main/java/org/choongang/member/controllers/JoinValidator.java
@@ -1,5 +1,6 @@
 package org.choongang.member.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.choongang.commons.validators.PasswordValidator;
 import org.choongang.member.repositories.MemberRepository;
@@ -13,6 +14,7 @@ import org.springframework.validation.Validator;
 public class JoinValidator implements Validator, PasswordValidator {
 
     private final MemberRepository memberRepository;
+    private final HttpSession session;
     @Override
     public boolean supports(Class<?> clazz) {
         return clazz.isAssignableFrom(RequestJoin.class);
@@ -24,6 +26,7 @@ public class JoinValidator implements Validator, PasswordValidator {
          * 1. 아이디, 이메일 중복여부 체크
          * 2. 비밀번호 복잡성 체크 : 대소문자 각각 1개이상, 숫자 1개이상, 특문 1개이상 포함
          * 3. 비밀번호, 비밀번호 확인 일치여부 체크
+         * 4. 이메일 인증 필수 여부 체크 EmailAuthVerified
          */
 
         RequestJoin form = (RequestJoin)target;
@@ -52,6 +55,12 @@ public class JoinValidator implements Validator, PasswordValidator {
         // 3. 비밀번호, 비밀번호 확인 일치여부 체크
         if(StringUtils.hasText(password) && StringUtils.hasText(confimPassword) && !password.equals(confimPassword)) {
             errors.rejectValue("confirmPassword", "Mismatch.password");
+        }
+
+        // 4. 이메일 인증 필수 여부 체크 EmailAuthVerified
+        boolean isVerified = (boolean) session.getAttribute("EmailAuthVerified");
+        if(!isVerified) {  //인증 x
+            errors.rejectValue("email", "Required.verified");
         }
     }
 }
