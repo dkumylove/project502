fd6d5a7d8ef8127561636fa9d41b6aadbad0225e
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-01-15 04:51:36 +0900

    upbit

---
+++ src/main/java/org/choongang/team3/service/UpbitService.java
@@ -0,0 +1,19 @@
+package org.choongang.team3.service;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.team3.entities.UpBitTicker;
+import org.choongang.team3.repositories.UpbitFeignClient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UpbitService {
+
+    private final UpbitFeignClient upbitFeignClient;
+
+    public List<UpBitTicker> getUpbitPrice(String markets){
+        return upbitFeignClient.getUpBitPriceList(markets);
+    }
+}
