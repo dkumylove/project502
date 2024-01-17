2464263d6f961a9d413db14dc43f51d48ad790c9
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-01-17 12:03:34 +0900

    게시글 삭제 작업중
    - 수정 삭제버튼을 누르면 인증기능 추가
      : password.html 생성
    
    - 일반적인 컨트롤러 에러를 처리 재정의 : BoardController/errorHandler()
    - BoardController/ passwordCheck() 추가
    - BoardAuthService
      : check() : 세션값 추가
      : validate() 추가

--- src/main/java/org/choongang/board/service/BoardAuthService.java
+++ src/main/java/org/choongang/board/service/BoardAuthService.java
@@ -1,16 +1,22 @@
 package org.choongang.board.service;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.choongang.board.entities.BoardData;
+import org.choongang.commons.Utils;
+import org.choongang.commons.exceptions.AlertException;
 import org.choongang.commons.exceptions.UnAuthorizedException;
 import org.choongang.member.entities.Member;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 @RequiredArgsConstructor
 public class BoardAuthService {
 
     private final BoardInfoService infoService;
+    private final HttpSession session;
 
     /**
      * 게시글 관련 권한 체크
@@ -25,6 +31,9 @@ public class BoardAuthService {
             Member member = data.getMember();
             // 비회원 -> 비밀번호 확인 필요
             if(member == null) {
+                session.setAttribute("mode", mode);
+                session.setAttribute("seq", seq);
+
                 throw new GuestPasswordCheckException();
             }
 
@@ -32,6 +41,21 @@ public class BoardAuthService {
             throw new UnAuthorizedException();
         }
 
+    }
+
+    /**
+     * 비회원 글수정, 글 삭제 비밀번호 확인
+     *
+     * @param password
+     */
+    public void validate(String password) {
+
+        if (!StringUtils.hasText(password)) {
+            throw new AlertException(Utils.getMessage("NotBlank.requestBoard.guestPw"), HttpStatus.BAD_REQUEST);
+        }
+
+        String mode = (String)session.getAttribute("mode");
+        Long seq = (Long)session.getAttribute("seq");
     }
 
 }
